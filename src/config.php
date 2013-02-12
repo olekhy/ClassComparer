a0e1afc75343ac5b9b1906c956eaa7e06730516b
Olekhy <olekhy@gmail.com>
2013-02-12 10:53:26 +0100

    Added config file

---
+++ src/config.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+    'error_reporting' => E_RECOVERABLE_ERROR | E_USER_ERROR | E_ERROR,
+    //'error_reporting' => -1,
+    'memory_limit' => '256M',
+
+    'blacklist' => array(
+        '.svn/',
+        'alice/public',
+        'bob/public',
+        'bob/data',
+        'conny/public',
+        'conny/data',
+        'log',
+        'logs',
+        'library/Apache',
+        'library/PHPExcel',
+        'library/Yii',
+        'library/Zend',
+        'vendor/tests',
+        'alice/vendor/tests'
+    ),
+    'directories' => array(
+        (isset($argv[1])) ? $argv[1]: '',
+        (isset($argv[2])) ? $argv[2]: '',
+    )
+);
